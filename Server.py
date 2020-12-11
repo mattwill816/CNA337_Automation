39b467f4b9b615bb8fe26ae4ee3ce7dfae8a8738
mattwill816 <65628421+mattwill816@users.noreply.github.com>
2020-12-10 20:52:59 -0800

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,14 +1,36 @@
-# This is the template code for the CNA337 Final Project
-# Matthew Williams
-# CNA 337 Fall 2020
-#Worked with Dylan McCormack,  Eric Yevenko
-#Received help from Hassan
-
+import os
+class Server:
+    """ Server class for representing and manipulating servers. """
     def __init__(self, server_ip):
-        # TODO -
-        self.server_ip = server_ip
 
+        self.server_ip = server_ip
     def ping(self):
-        # TODO - Use os module to ping the server
-        EC2Matt = os.system("ping -n 4 " + self.server_ip)
+
+        response = os.system("ping -n 4 " +self.server_ip)
         return self.server_ip
+
+# This is the template code for the CNA337 Final Project
+# CNA 337 Fall 2020
+# Matthew 12/10/2020
+# I got help from Hasan
+
+import paramiko
+user_name = "ubuntu"
+passwd = ""
+ip = "3.138.140.132"
+Private_key = r"new-key"
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(hostname=ip, username=user_name, password=passwd,key_filename=Private_key)
+
+stdin, stdout, stderr = ssh.exec_command('''sudo apt-get update
+                                         sudo apt-get upgrade -y
+                                         ''')
+stdin.flush()
+data = stdout.read().splitlines()
+for line in data:
+    print (line)
+
+
+ssh.close()
--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-# This is the template code for the CNA337 Final Project
-# Matthew Williams
 # CNA 337 Fall 2020
-# Worked with Dylan McCormack,  Eric Yevenko
-# Received help from Hassan
+# Matthew Williams 12/10/2020
+# I got help from Hasan
+
 from Server import Server
-
-
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Matthew")
 
+    print("Server Automator v0.1 by matthew")
 
-# This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    LaunchWizard26 = Server('3.138.140.132')
-    # TODO - Call Ping method and print the results
-    print(LaunchWizard26.ping())
+
+    EC2server = Server('3.138.140.132')
+
+    print(EC2server.ping())
